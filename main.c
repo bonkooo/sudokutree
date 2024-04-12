ff964da91183fd8a4651e3e2722f2b310d0fe689
bonkooo <143710575+bonkooo@users.noreply.github.com>
2024-04-12 22:00:11 +0200

    Update main.c

--- main.c
+++ main.c
@@ -2,12 +2,14 @@
 #include <stdlib.h>
 #include <math.h>
 #include <stdlib.h>
+#define MAX 100
 #define LEN 10
 
 typedef struct treeNode{
     int** info;
     int childrenSize;
     struct treeNode** children;
+    int dubina;
 } TreeNode;
 
 typedef struct tree{
@@ -34,6 +36,7 @@ TreeNode *kreirajCvor(int** matrix){
     cvor->childrenSize = 0;
     cvor->children = NULL;
     cvor->info = matrix;
+    cvor->dubina = 0;
 
     return cvor;
 }
@@ -44,13 +47,6 @@ int** alocirajMatricuStanja (int n){
         *(matrix + i) = (int*)malloc(n * sizeof(int));
     }
     return matrix;
-}
-
-void dealocirajMatricuStanja (int** matrix, int n){
-    for (int i = 0; i < n; i++){
-        free(matrix[i]);
-    }
-    free(matrix);
 }
 
 void push(stack *stek, struct treeNode *cvor) {
@@ -76,7 +72,7 @@ int stack_empty(stack *stek){
 
 struct treeNode *pop(stack *stek) {
     if (stack_empty(stek)) {
-        printf("Underflow\n");
+        //underflow
         return NULL;
     }
     stackElement *p = stek->sp;
@@ -183,7 +179,6 @@ void razvijStanja(stack* stek, struct treeNode* curr, int n){
                     }
                     novoStanje[i][j] = val;
                     dodajDete(stek, curr, n, novoStanje);
-                    dealocirajMatricuStanja(novoStanje, n);
                 }
             }
         }
@@ -199,11 +194,73 @@ void kreirajStablo(int** inicijalnaMatrica, struct tree *tree, int n){
     while(!stack_empty(stek)){
         TreeNode *curr = pop(stek);
         if (curr != NULL)
-        razvijStanja(stek, curr, n);
+            razvijStanja(stek, curr, n);
     }
 
 }
 
+void ispisStabla(struct tree* tree, int n){
+    stack* ispisStek = inicijalizujStek();
+
+    push(ispisStek, tree->root);
+    while (!stack_empty(ispisStek)){
+        struct treeNode* next = pop(ispisStek);
+        next->dubina = 0;
+        while (next != NULL){
+            for (int i = 0; i < n; i++){
+                for (int k = 0; k < next->dubina; k++){
+                    putchar('_');
+                }
+                for (int j = 0; j < n; j++){
+                    printf("%d ", next->info[i][j]);
+                }
+                putchar('\n');
+            }
+            putchar('\n');
+
+            for (int i = 0; i < next->childrenSize; i++){
+                next->children[i]->dubina = next->dubina + 1;
+                push(ispisStek, next->children[i]);
+            }
+
+            next = pop(ispisStek);
+        }
+    }
+}
+
+int proveriDobarPut(int **trenutnoStanje, struct tree* tree, int n){
+    stack* stekPut = inicijalizujStek();
+    push (stekPut, tree->root);
+    int trenPoklapanja = 0;
+
+    while(!stack_empty(stekPut)){
+        struct treeNode *next = pop(stekPut);
+        while (next != NULL){
+            trenPoklapanja = 0;
+            for (int i = 0; i < n; i++){
+                for (int j = 0; j < n; j++){
+                    if (trenutnoStanje[i][j] == next->info[i][j] || trenutnoStanje[i][j] == 0){
+                        trenPoklapanja++;
+                    }
+                }
+            }
+
+            if (trenPoklapanja == n*n && proveraValidneMatrice(next->info, n)){
+                return 1;
+            }
+
+            for (int i = 0; i < next->childrenSize; i++){
+                next->children[i]->dubina = next->dubina + 1;
+                push(stekPut, next->children[i]);
+            }
+
+            next = pop(stekPut);
+        }
+    }
+
+    return 0;
+
+}
 
 void ispisMatrice(int **matrix, int n){
     for (int i = 0; i < n; i++){
@@ -232,20 +289,102 @@ void odigrajPotez(int **matrix, int n, int i, int j, int num){
     }
 }
 
+int proveriResenje(int** matrica, int n){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            if (matrica[i][j] == 0){
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+int istaMatrica(int **matrica1, int **matrica2, int n){
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            if (matrica1[i][j] != matrica2[i][j]){
+                return 0;
+            }
+        }
+    }
+    return 1;
+}
+
+int proveriDuplikate(int **resenja[MAX], int **trenutnaMatrica, int len, int n){
+    for (int i = 0; i < len; i++){
+        if (istaMatrica(trenutnaMatrica, resenja[i], n)){
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void ispisiResenja(int **trenutnoStanje, struct tree* tree, int n) {
+    int** resenja[MAX];
+    int len = 0;
+    stack *stekPut = inicijalizujStek();
+    push(stekPut, tree->root);
+
+    while (!stack_empty(stekPut)) {
+        struct treeNode *next = pop(stekPut);
+        while (next != NULL) {
+            if (proveriResenje(next->info, n) && proveriDuplikate(resenja, next->info, len, n)) {
+                resenja[len++] = next->info;
+            }
+
+            for (int i = 0; i < next->childrenSize; i++) {
+                next->children[i]->dubina = next->dubina + 1;
+                push(stekPut, next->children[i]);
+            }
+
+            next = pop(stekPut);
+        }
+    }
+
+    for (int i = 0; i < len; i++){
+        ispisMatrice(resenja[i], n);
+        putchar('\n');
+    }
+
+}
+
+void predloziPotez (int** trenutnoStanje, struct tree* tree, int n){
+    if (!proveraValidneMatrice(trenutnoStanje, n)){
+        printf("Stanje nije validno.\n");
+        return;
+    }
+
+    struct tree *novoStablo = (struct tree*)malloc(sizeof(struct tree));
+    kreirajStablo(trenutnoStanje, novoStablo, n);
+
+    printf("Predlozen potez je: \n");
+    ispisMatrice(novoStablo->root->children[0]->info, n);
+
+}
 
 int main() {
     int n, **matrica, i, j, num;
     char opcija;
 
+    struct tree *inicijalnoStablo = (struct tree*)malloc(sizeof(struct tree));
+    if (!inicijalnoStablo) {
+        printf("Greska u alokaciji memorije\n");
+    }
+
+
+    inicijalnoStablo ->root = NULL;
     do {
         printf("Meni: \n");
         printf("a - Unesi inicijalno stanje\n");
         printf("b - Ispis matrice\n");
         printf("c - Odigraj potez\n");
-        printf("d - Ispisi matricu\n");
+        printf("d - Proveri da li si na dobrom putu\n");
         printf("f - Trazi pomoc\n");
-        printf("g - Proveri da li si na dobrom putu\n");
         printf("h - Proveri validno stanje\n");
+        printf("i - Formiraj stablo\n");
+        printf("j - Ispisi stablo\n");
+        printf("k - Ispisi resenja\n");
         printf("e - Izlaz\n");
         scanf(" %c", &opcija);
 
@@ -284,26 +423,50 @@ int main() {
                 break;
             case 'c':
                 ispisMatrice(matrica, n);
-                printf("Izaberite polje u koje zelite da upisete broj [i, j]: ");
+                printf("Izaberite polje u koje zelite da upisete broj [i, j]:\n");
                 scanf(" %d%d", &i, &j );
                 putchar('\n');
-                printf("Izaberite broj koji zelite da upisete: ");
+                printf("Izaberite broj koji zelite da upisete:\n");
                 scanf("%d", &num);
                 putchar('\n');
                 odigrajPotez(matrica, n, i, j, num);
                 break;
             case 'd':
+                if (proveriDobarPut(matrica, inicijalnoStablo, n)){
+                    printf("Na dobrom ste putu. \n");
+                }
+                else{
+                    printf("Niste na dobrom putu. \n");
+                }
+                break;
+            case 'f':
+                predloziPotez(matrica, inicijalnoStablo, n);
                 break;
             case 'e':
                 printf("Izlaz\n");
                 break;
             case 'h':
                 if (proveraValidneMatrice(matrica, n)){
-                    printf("Matrica je validna. ");
+                    printf("Matrica je validna. \n");
                 }
                 else{
-                    printf("Matrica nije validna. ");
+                    printf("Matrica nije validna. \n");
                 }
+                break;
+            case 'i':
+                if (!inicijalnoStablo->root){
+                    kreirajStablo(matrica, inicijalnoStablo, n);
+                    printf("Stablo formirano.\n");
+                }
+                break;
+            case 'j':
+                if (!inicijalnoStablo->root){
+                    printf("Stablo nije formirano.\n");
+                }
+                ispisStabla(inicijalnoStablo, n);
+                break;
+            case 'k':
+                ispisiResenja(matrica, inicijalnoStablo, n);
                 break;
             default:
                 printf("Nepoznata opcija\n");
